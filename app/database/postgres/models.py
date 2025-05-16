7e5717df0f1ba661627accd9ea921b5c818e7cbb
Vadim Streltsov <67193820+VadimStr315@users.noreply.github.com>
2025-05-16 12:41:35 +0500

    rewrite models and add new feature

--- app/database/postgres/models.py
+++ app/database/postgres/models.py
@@ -1,8 +1,8 @@
 import datetime
 from typing import Annotated, List
-from sqlalchemy import ForeignKey, Integer, String, Date, Boolean, Float, UniqueConstraint, UUID
+from sqlalchemy import ForeignKey, Integer, String,Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
-from sqlalchemy.orm import validates
+from sqlalchemy.ext.declarative import declarative_base
 
 
 intpk = Annotated[int, mapped_column(primary_key=True)]
@@ -25,14 +25,19 @@ class Base(DeclarativeBase):
 
         return f"<{self.__class__.__name__} {', '.join(cols)}>"
 
+card_collection_association = Table(
+    'card_collection', Base.metadata,
+    Column('card_id', Integer, ForeignKey('cards.id'), primary_key=True),
+    Column('collection_id', Integer, ForeignKey('collections.id'), primary_key=True)
+)
 
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[intpk]
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
-    email: Mapped[str_256]
-    hashed_password: Mapped[str_256]
+    email: Mapped[str] = mapped_column(String(256))
+    hashed_password: Mapped[str] = mapped_column(String(256))
 
     repr_cols_num = 2
     repr_cols = ("email",)
@@ -47,7 +52,7 @@ class Collections(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
-    name:  Mapped[str] = mapped_column(String(255),unique=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
 
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     
@@ -55,10 +60,11 @@ class Collections(Base):
     repr_cols = ("name", "amount_of_cards")
 
     cards: Mapped[List["Cards"]] = relationship(
-    "Cards", back_populates="collection")
+        "Cards", secondary=card_collection_association, back_populates="collections"
+    )
 
-    user: Mapped["User"] = relationship(
-        "User", back_populates="collections"
+    user: Mapped["User "] = relationship(
+        "User ", back_populates="collections"
     )
 
 
@@ -67,8 +73,8 @@ class Cards(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
 
-    text:  Mapped[str] = mapped_column(String)
-    collection_id: Mapped[int] = mapped_column(
-        ForeignKey('collections.id', ondelete='CASCADE'))
-    collection: Mapped[Collections] = relationship(
-        'Collections', back_populates='cards') 
+    text: Mapped[str] = mapped_column(String)
+
+    collections: Mapped[List[Collections]] = relationship(
+        "Collections", secondary=card_collection_association, back_populates="cards"
+    )
