ebc5ba2dc0d77489ae25d9c928ab9fe85c5a14ad
artemmel401 <125571136+artemmel401@users.noreply.github.com>
2025-05-21 17:49:15 +0500

    add focus

--- ui/src/services/token.ts
+++ ui/src/services/token.ts
@@ -1,30 +1,13 @@
 export function getTokenFromCookie(name: 'access' | 'refresh') {
-  const matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + `${name}_token`.replace(/([\\.$?*|{}\\(\\)\\[\]\\\\/\\+^])/g, '\\$1') + "=([^;]*)"));
-  return matches ? decodeURIComponent(matches[1]) : undefined;
+  return localStorage.getItem(`${name}_token`);
 }
 
 
 export function setTokensToCookies(token: string, tokenType: 'access' | 'refresh', maxAge?:number) {
-  const maxAgeRefresh = import.meta.env.VITE_EXPIRE_REFRESH_TOKEN ? import.meta.env.VITE_EXPIRE_REFRESH_TOKEN : 172800
-  const maxAgeAccess = import.meta.env.VITE_EXPIRE_ACCESS_TOKEN ? import.meta.env.VITE_EXPIRE_ACCESS_TOKEN : 7200
-  const options:Record<string, string | number | boolean> = {
-    path: '/',
-    secure: import.meta.env.VITE_USE_SECURE === "1", 
-    'max-age': maxAge ? maxAge : tokenType === 'access' ? maxAgeAccess : maxAgeRefresh
-  };
-  let updatedCookie = encodeURIComponent(`${tokenType}_token`) + "=" + encodeURIComponent(token);
-  for (const optionKey in options) {
-    updatedCookie += "; " + optionKey;
-    const optionValue = options[optionKey];
-    if (optionValue !== true) {
-      updatedCookie += "=" + optionValue;
-    }
-  }
-
-  document.cookie = updatedCookie;
+  console.log(maxAge)
+  localStorage.setItem(`${tokenType}_token`, token);
 }
 
 export function removeTokensFromCookies(tokenType: 'access' | 'refresh') {
-  setTokensToCookies("",`${tokenType}`, -1)
+  localStorage.removeItem(`${tokenType}_token`);
 }
